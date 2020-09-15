79b3a59bb2f2be7c2631d78ada673f54fc8b0694
Russell Tucker <russtuck91@gmail.com>
2020-09-15 18:14:05 -0400

    Add upgrade script for rule comparators on playlist rules

---
+++ api/scripts/dbupgrade/upgrade-1.0.0-2.0.0-add-rule-comparators.ts
@@ -0,0 +1,59 @@
+import { isPlaylistRuleGroup, PlaylistRule, PlaylistRuleGroup, RuleComparator, RuleParam } from '../../../shared';
+import { db } from '../../src/core/db/db';
+
+
+function addComparatorToRuleGroups(rules: PlaylistRuleGroup[]): PlaylistRuleGroup[] {
+    return rules.map((rule) => addComparatorToRuleGroup(rule));
+}
+
+function addComparatorToRuleGroup(rule: PlaylistRuleGroup): PlaylistRuleGroup {
+    return {
+        ...rule,
+        rules: addComparatorToEither(rule.rules)
+    };
+}
+
+function addComparatorToEither(rules: (PlaylistRule|PlaylistRuleGroup)[]): (PlaylistRule|PlaylistRuleGroup)[] {
+    return rules.map((rule) => {
+        if (isPlaylistRuleGroup(rule)) {
+            return addComparatorToRuleGroup(rule);
+        } else {
+            return addComparatorToRule(rule);
+        }
+    });
+}
+
+function addComparatorToRule(rule: PlaylistRule): PlaylistRule {
+    switch (rule.param) {
+        case RuleParam.Artist:
+        case RuleParam.Album:
+        case RuleParam.Track:
+            rule.comparator = RuleComparator.Contains;
+            break;
+        case RuleParam.Genre:
+        case RuleParam.Year:
+            rule.comparator = RuleComparator.Is;
+            break;
+    }
+
+    return rule;
+}
+
+async function addRuleComparators() {
+    try {
+        const playlists = await db.playlists.find();
+        for (const playlist of playlists) {
+            playlist.rules = addComparatorToRuleGroups(playlist.rules);
+            await db.playlists.update(
+                { _id: playlist._id },
+                { $set: playlist }
+            );
+        }
+    } catch (e) {
+        console.log('Error in addRuleComparators');
+        console.log(e);
+        throw e;
+    }
+}
+export default addRuleComparators;
+
