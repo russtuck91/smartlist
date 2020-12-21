23d7cc981e79e02ed26e37744f03a45083888651
Russell Tucker <russtuck91@gmail.com>
2020-12-20 22:33:47 -0500

    Move common logging components to shared dir

---
+++ shared/src/util/logger/logger.ts
@@ -0,0 +1,26 @@
+import winston from 'winston';
+
+export function getLogLevel() {
+    console.log('>>>> Entering getLogLevel()');
+    if (process.env.LOG_LEVEL) {
+        console.log('env LOG_LEVEL:', process.env.LOG_LEVEL);
+        return process.env.LOG_LEVEL;
+    }
+    if (process.env.NODE_ENV === 'development') {
+        console.log('env NODE_ENV==development, setting level to debug');
+        return 'debug';
+    }
+    console.log('No log level condition met, using default level');
+    return undefined;
+}
+
+export const prettyLogFormat = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss ZZ' }),
+    winston.format.align(),
+    winston.format.printf((info) => {
+        const { timestamp, level, message, ...args } = info;
+        return `${timestamp} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+    }),
+);
+
