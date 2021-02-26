9bf924969973ba022b6cd26e53f8acb7b251af3d
Russell Tucker <russtuck91@gmail.com>
2021-02-26 12:51:30 -0500

    Add WebView to native app render

--- native/App.tsx
+++ native/App.tsx
@@ -1,29 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {
     SafeAreaView,
-    ScrollView,
     StatusBar,
     StyleSheet,
     Text,
     View,
 } from 'react-native';
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { WebView } from 'react-native-webview';
 
 declare const global: {HermesInternal: null | Record<string, unknown>};
 
@@ -31,79 +15,34 @@ const App = () => {
     return (
         <>
             <StatusBar barStyle="dark-content" />
-            <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}
-                >
-                    <Header />
-                    {global.HermesInternal == null ? null : (
-                        <View style={styles.engine}>
-                            <Text style={styles.footer}>Engine: Hermes</Text>
-                        </View>
-                    )}
-                    <View style={styles.body}>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Step One</Text>
-                            <Text style={styles.sectionDescription}>
-                                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                                this screen and then come back to see your edits.
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>See Your Changes</Text>
-                            <Text style={styles.sectionDescription}>
-                                <ReloadInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Debug</Text>
-                            <Text style={styles.sectionDescription}>
-                                <DebugInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Learn More</Text>
-                            <Text style={styles.sectionDescription}>
-                                Read the docs to discover what to do next:
-                            </Text>
-                        </View>
-                        <LearnMoreLinks />
+            <SafeAreaView style={styles.safeAreaView}>
+                {global.HermesInternal == null ? null : (
+                    <View style={styles.engine}>
+                        <Text style={styles.footer}>Engine: Hermes</Text>
                     </View>
-                </ScrollView>
+                )}
+                <WebView
+                    source={{
+                        uri: 'https://smart--list.herokuapp.com/',
+                    }}
+                    style={styles.webview}
+                />
             </SafeAreaView>
         </>
     );
 };
 
 const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
+    safeAreaView: {
+        flex: 1,
     },
     engine: {
         position: 'absolute',
         right: 0,
     },
-    body: {
-        backgroundColor: Colors.white,
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
+    webview: {
+        flex: 1,
+        minHeight: 400,
     },
     footer: {
         color: Colors.dark,
