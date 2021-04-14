0fedeabe1c46762e5c6109d75aa3550d72aafed7
Russell Tucker <russtuck91@gmail.com>
2021-04-14 19:25:04 -0400

    Add SecondaryAppBar to Account page

--- web/src/account/account.tsx
+++ web/src/account/account.tsx
@@ -1,7 +1,8 @@
-import { Button, CircularProgress, Container, Link } from '@material-ui/core';
+import { Box, Button, CircularProgress, Container, Link, Typography } from '@material-ui/core';
 import * as React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
+import { SecondaryAppBar } from '../core/components/secondary-app-bar';
 import { baseRequestUrl, requests } from '../core/requests/requests';
 import { RouteLookup } from '../core/routes/route-lookup';
 
@@ -20,42 +21,49 @@ export class Account extends React.Component<any, AccountState> {
         const { userInfo } = this.state;
 
         return (
-            <Container>
-                <div>
-                    {!userInfo ? (<CircularProgress />) : (
-                        <>
-                            <h1>
-                                Logged in as
-                                {' '}
-                                {userInfo.display_name}
-                            </h1>
-                            {userInfo.images && userInfo.images.length > 0 ? (
-                                <div><img src={userInfo.images[0].url} /></div>
-                            ) : null}
-                            <div>
-                                Display name:
-                                {' '}
-                                {userInfo.display_name}
-                            </div>
-                            <div>
-                                Id:
-                                {' '}
-                                {userInfo.id}
-                            </div>
-                            <div>
-                                Email:
-                                {' '}
-                                {userInfo.email}
-                            </div>
-                            <p>
-                                <Link component={RouterLink} underline="none" to={RouteLookup.logout}>
-                                    <Button variant="contained" color="primary">Log Out</Button>
-                                </Link>
-                            </p>
-                        </>
-                    )}
-                </div>
-            </Container>
+            <Box display="flex" flex="1 1 auto" flexDirection="column">
+                <SecondaryAppBar>
+                    <Typography variant="h6">
+                        Account
+                    </Typography>
+                </SecondaryAppBar>
+                <Container>
+                    <Box>
+                        {!userInfo ? (<CircularProgress />) : (
+                            <>
+                                <h2>
+                                    Logged in as
+                                    {' '}
+                                    {userInfo.display_name}
+                                </h2>
+                                {userInfo.images && userInfo.images.length > 0 ? (
+                                    <div><img src={userInfo.images[0].url} /></div>
+                                ) : null}
+                                <div>
+                                    Display name:
+                                    {' '}
+                                    {userInfo.display_name}
+                                </div>
+                                <div>
+                                    ID:
+                                    {' '}
+                                    {userInfo.id}
+                                </div>
+                                <div>
+                                    Email:
+                                    {' '}
+                                    {userInfo.email}
+                                </div>
+                                <p>
+                                    <Link component={RouterLink} underline="none" to={RouteLookup.logout}>
+                                        <Button variant="contained" color="primary">Log Out</Button>
+                                    </Link>
+                                </p>
+                            </>
+                        )}
+                    </Box>
+                </Container>
+            </Box>
         );
     }
 
