78f67ceef29959b14e653ec2acd2e77ba7f6aa91
Russell Tucker <russtuck91@gmail.com>
2021-08-15 00:06:20 -0400

    Add trackService, with method getTrackDetails

---
+++ api/src/services/track-service/get-track-details.ts
@@ -0,0 +1,31 @@
+import { TrackDetails } from '../../../../shared';
+
+import logger from '../../core/logger/logger';
+
+import spotifyCacheService from '../cache/spotify/spotify-cache-service';
+import spotifyService from '../spotify-service/spotify-service';
+
+
+async function getTrackDetails(id: string, accessToken: string|undefined): Promise<TrackDetails> {
+    logger.debug(`>>>> Entering getTrackDetails(id = ${id}`);
+
+    const track = await spotifyService.getTrackById(id, accessToken);
+
+    const albumIds = [track.album.id];
+    const artistIds = track.artists.map((a) => a.id);
+    const albums = await spotifyCacheService.getAlbums(albumIds, accessToken);
+    const artists = await spotifyCacheService.getArtists(artistIds, accessToken);
+
+    const albumGenres = albums.reduce<string[]>((agg, album) => agg.concat(album.genres), []);
+    const artistGenres = artists.reduce<string[]>((agg, artist) => agg.concat(artist.genres), []);
+    const genres = [...new Set([...artistGenres, ...albumGenres])];
+
+    const audioFeatures = await spotifyCacheService.getAudioFeatures([id], accessToken);
+
+    return {
+        genres,
+        audioFeatures: audioFeatures[0],
+    };
+}
+
+export default getTrackDetails;
---
+++ api/src/services/track-service/index.ts
@@ -0,0 +1,5 @@
+import getTrackDetails from './get-track-details';
+
+export {
+    getTrackDetails,
+};
