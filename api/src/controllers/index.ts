add4b4235e747b0fd677679d381c5de4fbf24379
Russell Tucker <russtuck91@gmail.com>
2021-08-15 00:06:44 -0400

    Add TrackController for querying track details

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -2,4 +2,5 @@ export * from './log-controller';
 export * from './login-controller';
 export * from './playlists-controller';
 export * from './search-controller';
+export * from './track-controller';
 export * from './user-controller';
---
+++ api/src/controllers/track-controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Wrapper } from '@overnightjs/core';
+import { Request, Response } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+
+import { doAndRetryWithCurrentUser } from '../core/session/session-util';
+
+import * as trackService from '../services/track-service';
+
+
+@Controller('track')
+export class TrackController {
+    @Get(':id/details')
+    @Wrapper(expressAsyncHandler)
+    async getTrackDetails(req: Request, res: Response) {
+        await doAndRetryWithCurrentUser(async (accessToken) => {
+            const details = await trackService.getTrackDetails(req.params.id, accessToken);
+
+            res.send(details);
+        });
+    }
+}
