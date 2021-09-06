b01dfa0731d1d57de73e9ed16e1afd337ebf43a9
Russell Tucker <russtuck91@gmail.com>
2021-09-06 02:09:20 -0400

    Add wait for rate limit in getTracksForPlaylist

--- api/src/services/spotify-service/get-tracks-for-playlist.ts
+++ api/src/services/spotify-service/get-tracks-for-playlist.ts
@@ -1,5 +1,6 @@
 import logger from '../../core/logger/logger';
 
+import doAndWaitForRateLimit from './do-and-wait-for-rate-limit';
 import getFullPagedResults from './get-full-paged-results';
 import initSpotifyApi from './init-spotify-api';
 import { SpResponse } from './types';
@@ -10,10 +11,12 @@ async function getTracksForPlaylist(playlistId: string, accessToken: string|unde
 
     const spotifyApi = await initSpotifyApi(accessToken);
 
-    const result: SpotifyApi.PagingObject<SpotifyApi.PlaylistTrackObject>|undefined = await getFullPagedResults(async (options) => {
-        const response: SpResponse<SpotifyApi.PlaylistTrackResponse> = await spotifyApi.getPlaylistTracks(playlistId, options);
-        return response.body;
-    });
+    const result: SpotifyApi.PagingObject<SpotifyApi.PlaylistTrackObject>|undefined = await getFullPagedResults(
+        async (options) => await doAndWaitForRateLimit(async () => {
+            const response: SpResponse<SpotifyApi.PlaylistTrackResponse> = await spotifyApi.getPlaylistTracks(playlistId, options);
+            return response.body;
+        }),
+    );
     if (!result) return [];
     // Despite documentation, `track` can sometimes be null
     const resultTracks: (SpotifyApi.TrackObjectFull|null)[] = result.items.map((item) => item.track);
