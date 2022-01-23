1e260b343e5e4441039bdab973f75acf449bf553
Russell Tucker <russtuck91@gmail.com>
2022-01-23 18:05:02 -0500

    Add TODO comment

--- api/src/services/cache/db-cache-service.ts
+++ api/src/services/cache/db-cache-service.ts
@@ -7,6 +7,8 @@ import CacheRepository from '../../repositories/cache/cache-repository';
 
 import { SourceMethod } from './types';
 
+// TODO ensureIndex on item.id
+
 
 class DbCacheService<Resource extends CacheableResource> {
     repo: CacheRepository<Resource>;
