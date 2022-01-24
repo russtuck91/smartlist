6383d79a01ffb910598a3788820cce8a6e121d78
Russell Tucker <russtuck91@gmail.com>
2022-01-24 15:38:24 -0500

    Add trackCacheService methods to spotifyCacheService

--- api/src/services/cache/spotify/spotify-cache-service.ts
+++ api/src/services/cache/spotify/spotify-cache-service.ts
@@ -2,12 +2,15 @@ import albumCacheService from './album-cache-service';
 import artistCacheService from './artist-cache-service';
 import audioFeaturesCacheService from './audio-features-cache-service';
 import searchForGenre from './search-for-genre';
+import trackCacheService from './track-cache-service';
 import userSavedTracksCacheService from './user-saved-tracks-cache-service';
 
 
 class SpotifyCacheService {
     getFullMySavedTracks = userSavedTracksCacheService.getFullList;
 
+    getTracks = trackCacheService.getItems;
+    setTracks = trackCacheService.setItems;
     getAlbums = albumCacheService.getItems;
     getArtists = artistCacheService.getItems;
     getAudioFeatures = audioFeaturesCacheService.getItems;
