f9e79f57526318b9be95a9ac679915b68199c721
Russell Tucker <russtuck91@gmail.com>
2022-01-27 19:40:55 -0500

    Add secondary banner to login page

--- web/src/login/login.tsx
+++ web/src/login/login.tsx
@@ -1,7 +1,8 @@
-import { Button, Container, Link } from '@material-ui/core';
+import { Box, Button, Container, Link, Typography } from '@material-ui/core';
 import * as React from 'react';
 import { Route } from 'react-router';
 
+import { SecondaryAppBar } from '../core/components/secondary-app-bar';
 import { baseRequestUrl } from '../core/requests/requests';
 import { RouteLookup } from '../core/routes/route-lookup';
 
@@ -11,13 +12,20 @@ import { LoginError } from './login-error';
 export class Login extends React.Component {
     render() {
         return (
-            <Container>
-                <h2>Login with Spotify to get started</h2>
-                <Link href={`${baseRequestUrl}/login`} underline="none">
-                    <Button variant="contained" color="primary">Login with Spotify</Button>
-                </Link>
-                <Route path={RouteLookup.login.error} component={LoginError} />
-            </Container>
+            <Box display="flex" flex="1 1 auto" flexDirection="column">
+                <SecondaryAppBar>
+                    <Typography variant="h6">
+                        Login
+                    </Typography>
+                </SecondaryAppBar>
+                <Container>
+                    <h2>Login with Spotify to get started</h2>
+                    <Link href={`${baseRequestUrl}/login`} underline="none">
+                        <Button variant="contained" color="primary">Login with Spotify</Button>
+                    </Link>
+                    <Route path={RouteLookup.login.error} component={LoginError} />
+                </Container>
+            </Box>
         );
     }
 }
