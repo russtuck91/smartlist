b10d184cefa5358e597a16df39595c08ef9396dc
Russell Tucker <russtuck91@gmail.com>
2022-02-19 16:59:27 -0500

    Extract content to PlaylistPreviewArea and wrap with ErrorBoundary

--- web/src/playlists/playlist-builder/playlist-builder-form.tsx
+++ web/src/playlists/playlist-builder/playlist-builder-form.tsx
@@ -1,5 +1,4 @@
 import {
-    Avatar,
     Box, Button, CircularProgress,
     Container, Grid, IconButton, List,
     StyleRules,
@@ -8,18 +7,14 @@ import {
     WithStyles, withStyles, WithWidth, withWidth,
 } from '@material-ui/core';
 import { ArrowBack } from '@material-ui/icons';
-import { Alert, Skeleton } from '@material-ui/lab';
 import { FormikProps } from 'formik';
 import { isEmpty, isEqual } from 'lodash';
 import * as React from 'react';
-import LazyLoad from 'react-lazyload';
 
 import { PlaylistRuleGroup, RuleGroupType, Track } from '../../../../shared';
 
-import Ellipsis from '../../core/components/ellipsis';
 import { SecondaryAppBar } from '../../core/components/secondary-app-bar';
-import { VirtualTableRenderer } from '../../core/components/tables';
-import { ColumnFormatType, ColumnSet, CustomCellFormatterProps } from '../../core/components/tables/models';
+import { ErrorBoundary } from '../../core/errors/error-boundary';
 import { TextField } from '../../core/forms/fields';
 import { history } from '../../core/history/history';
 import logger from '../../core/logger/logger';
@@ -30,9 +25,9 @@ import { Nullable } from '../../core/shared-models/types';
 import { PlaylistContainer } from '../playlist-container';
 
 import { DEFAULT_NEW_CONDITION, PlaylistBuilderFormValues } from './models';
+import PlaylistPreviewArea from './playlist-preview-area';
 import { RuleGroup } from './rule-group';
 import TabPanel from './tab-panel';
-import { TrackRowDetails } from './track-row-details';
 
 
 export interface PlaylistBuilderFormProps {
@@ -89,27 +84,6 @@ const useStyles = (theme: Theme): StyleRules => ({
             padding: 0,
         },
     },
-    previewArea: {
-        '& .MuiTable-root': {
-            tableLayout: 'fixed',
-        },
-        '& .MuiTableCell-root': {
-            width: '100%',
-            color: theme.palette.text.secondary,
-        },
-        '& .MuiAvatar-root': {
-            '& img': {
-                maxWidth: '100%',
-            },
-        },
-        '& .MuiSkeleton-root': {
-            '& .MuiAvatar-root': {
-                '& .MuiAvatar-fallback': {
-                    display: 'none',
-                },
-            },
-        },
-    },
 });
 
 type FullProps = PlaylistBuilderFormProps & WithStyles<typeof useStyles> & WithWidth;
@@ -119,11 +93,6 @@ export class RawPlaylistBuilderForm extends React.Component<FullProps, PlaylistB
         listPreview: [],
         selectedTab: 0,
     };
-
-    private listPreviewColumnSet: ColumnSet<Track> = [
-        { title: 'Name', mapsToField: 'name', type: ColumnFormatType.TrackName, width: '60%' },
-        { title: 'Album', mapsToField: 'albumName', type: ColumnFormatType.Ellipsis, width: '40%' },
-    ];
 
     async componentDidMount() {
         await this.props.formik.validateForm();
@@ -303,69 +272,11 @@ export class RawPlaylistBuilderForm extends React.Component<FullProps, PlaylistB
 
     private renderPreviewArea() {
         return (
-            <Box className={this.props.classes.previewArea} flex="1 1 auto" display="flex" justifyContent="center">
-                {this.renderPreviewContent()}
-            </Box>
-        );
-    }
-
-    private renderPreviewContent() {
-        const { listPreview } = this.state;
-
-        if (listPreview === null) {
-            return (
-                <Box>
-                    <Alert severity="error">There was a problem loading the playlist. Please try again.</Alert>
-                </Box>
-            );
-        }
-
-        return (
-            <VirtualTableRenderer
-                data={listPreview || []}
-                columns={this.listPreviewColumnSet}
-
-                isLoading={listPreview === undefined}
-                customCellFormatter={this.cellFormatter}
-                footerLabel="tracks"
-                expandableRows={{
-                    renderExpandedRow: this.renderExpandedRow,
-                }}
-            />
-        );
-    }
-
-    private cellFormatter = ({ cellValue, column, rowData, isLoading }: CustomCellFormatterProps<Track>) => {
-        if (column.type === ColumnFormatType.TrackName) {
-            return (
-                <Grid container wrap="nowrap" spacing={1}>
-                    <Grid item>
-                        {isLoading ? (
-                            <Skeleton variant="rect">
-                                <Avatar variant="square" />
-                            </Skeleton>
-                        ) : (
-                            <Avatar variant="square">
-                                <LazyLoad overflow offset={5000}>
-                                    <img src={rowData.albumThumbnail} />
-                                </LazyLoad>
-                            </Avatar>
-                        )}
-                    </Grid>
-                    <Grid item xs style={{ overflow: 'hidden' }}>
-                        {isLoading ? <Skeleton /> : <Typography color="textPrimary"><Ellipsis>{cellValue}</Ellipsis></Typography>}
-                        {isLoading ? <Skeleton /> : <Ellipsis>{rowData.artistNames[0]}</Ellipsis>}
-                    </Grid>
-                </Grid>
-            );
-        }
-    }
-
-    private renderExpandedRow(rowData: Track) {
-        return (
-            <TrackRowDetails
-                track={rowData}
-            />
+            <ErrorBoundary>
+                <PlaylistPreviewArea
+                    listPreview={this.state.listPreview}
+                />
+            </ErrorBoundary>
         );
     }
 
---
+++ web/src/playlists/playlist-builder/playlist-preview-area.tsx
@@ -0,0 +1,123 @@
+import { Avatar, Box, Grid, StyleRules, Theme, Typography, WithStyles, withStyles } from '@material-ui/core';
+import { Alert, Skeleton } from '@material-ui/lab';
+import React from 'react';
+import LazyLoad from 'react-lazyload';
+
+import { Track } from '../../../../shared';
+
+import Ellipsis from '../../core/components/ellipsis';
+import { VirtualTableRenderer } from '../../core/components/tables';
+import { ColumnFormatType, ColumnSet, CustomCellFormatterProps } from '../../core/components/tables/models';
+import { Nullable } from '../../core/shared-models/types';
+
+import { TrackRowDetails } from './track-row-details';
+
+
+interface PlaylistPreviewAreaProps {
+    listPreview?: Nullable<Track[]>;
+}
+
+const useStyles = (theme: Theme): StyleRules => ({
+    previewArea: {
+        '& .MuiTable-root': {
+            tableLayout: 'fixed',
+        },
+        '& .MuiTableCell-root': {
+            width: '100%',
+            color: theme.palette.text.secondary,
+        },
+        '& .MuiAvatar-root': {
+            '& img': {
+                maxWidth: '100%',
+            },
+        },
+        '& .MuiSkeleton-root': {
+            '& .MuiAvatar-root': {
+                '& .MuiAvatar-fallback': {
+                    display: 'none',
+                },
+            },
+        },
+    },
+});
+
+type FullProps = PlaylistPreviewAreaProps & WithStyles<typeof useStyles>;
+
+class RawPlaylistPreviewArea extends React.Component<FullProps> {
+    private listPreviewColumnSet: ColumnSet<Track> = [
+        { title: 'Name', mapsToField: 'name', type: ColumnFormatType.TrackName, width: '60%' },
+        { title: 'Album', mapsToField: 'albumName', type: ColumnFormatType.Ellipsis, width: '40%' },
+    ];
+
+    render() {
+        return (
+            <Box className={this.props.classes.previewArea} flex="1 1 auto" display="flex" justifyContent="center">
+                {this.renderPreviewContent()}
+            </Box>
+        );
+    }
+
+    private renderPreviewContent() {
+        const { listPreview } = this.props;
+
+        if (listPreview === null) {
+            return (
+                <Box>
+                    <Alert severity="error">There was a problem loading the playlist. Please try again.</Alert>
+                </Box>
+            );
+        }
+
+        return (
+            <VirtualTableRenderer
+                data={listPreview || []}
+                columns={this.listPreviewColumnSet}
+
+                isLoading={listPreview === undefined}
+                customCellFormatter={this.cellFormatter}
+                footerLabel="tracks"
+                expandableRows={{
+                    renderExpandedRow: this.renderExpandedRow,
+                }}
+            />
+        );
+    }
+
+    private cellFormatter = ({ cellValue, column, rowData, isLoading }: CustomCellFormatterProps<Track>) => {
+        if (column.type === ColumnFormatType.TrackName) {
+            return (
+                <Grid container wrap="nowrap" spacing={1}>
+                    <Grid item>
+                        {isLoading ? (
+                            <Skeleton variant="rect">
+                                <Avatar variant="square" />
+                            </Skeleton>
+                        ) : (
+                            <Avatar variant="square">
+                                <LazyLoad overflow offset={5000}>
+                                    <img src={rowData.albumThumbnail} />
+                                </LazyLoad>
+                            </Avatar>
+                        )}
+                    </Grid>
+                    <Grid item xs style={{ overflow: 'hidden' }}>
+                        {isLoading ? <Skeleton /> : <Typography color="textPrimary"><Ellipsis>{cellValue}</Ellipsis></Typography>}
+                        {isLoading ? <Skeleton /> : <Ellipsis>{rowData.artistNames[0]}</Ellipsis>}
+                    </Grid>
+                </Grid>
+            );
+        }
+    }
+
+    private renderExpandedRow(rowData: Track) {
+        return (
+            <TrackRowDetails
+                track={rowData}
+            />
+        );
+    }
+}
+
+const PlaylistPreviewArea = withStyles(useStyles)(RawPlaylistPreviewArea);
+
+export default PlaylistPreviewArea;
