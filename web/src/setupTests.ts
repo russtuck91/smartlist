5d851247350692b1b11ebeac67b64918320fe8f0
Russell Tucker <russtuck91@gmail.com>
2022-03-07 22:27:48 -0500

    Add mocks for fix for web tests

---
+++ web/src/setupTests.ts
@@ -0,0 +1,8 @@
+
+
+
+jest.mock('react-ga');
+
+// Export empty object to adhere to isolatedModules flag. ts(1208)
+// If this file ever imports or exports something real, then this can be removed.
+export {};
