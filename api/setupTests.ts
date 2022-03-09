61d3cf29e0f025d0dcadbdc8ff06e50e8d0c7644
Russell Tucker <russtuck91@gmail.com>
2022-03-08 22:38:25 -0500

    Inject jest globals in setupTests

--- api/setupTests.ts
+++ api/setupTests.ts
@@ -1,4 +1,10 @@
+import jestGlobals from '@jest/globals';
 import { jest } from '@jest/globals';
 
 
+Object.entries(jestGlobals).map(([key, value]) => {
+    global[key] = value;
+});
+
 jest.mock('mongtype');
+
