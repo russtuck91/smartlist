93f4293dc666cd3a5ea07a1462efda21886fb4c4
Russell Tucker <russtuck91@gmail.com>
2022-06-22 23:05:18 -0400

    Update MainActivity.java

--- native/android/app/src/main/java/com/smartlistmusic/smartlist/MainActivity.java
+++ native/android/app/src/main/java/com/smartlistmusic/smartlist/MainActivity.java
@@ -1,12 +1,8 @@
 package com.smartlistmusic.smartlist;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate; // <- add this necessary import
-//import com.facebook.react.ReactRootView;
-import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
-// import org.devio.rn.splashscreen.SplashScreen;
-//import androidx.core.splashscreen.SplashScreen;
-import android.os.Bundle;
+import com.facebook.react.ReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -19,29 +15,12 @@ public class MainActivity extends ReactActivity {
     return "SmartList";
   }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    // OLD way - this makes the "custom" xml splash screen show after the standard one. Does stick around til the WebView loads
-    // SplashScreen.show(this);
-  
-    // SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
-  
-    super.onCreate(savedInstanceState);
-    // installSplashScreen();
-    // setContentView(R.layout.main_activity);
-
-    // Attempt to keep splash screen up for X seconds
-    // splashScreen.setKeepOnScreenCondition(() -> keep);
-    // Handler handler = new Handler();
-    // handler.postDelayed(() -> keep = false, 60 * 1000);
-  }
-
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
 
       @Override
       protected void loadApp(String appKey) {
-        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+        RNBootSplash.init(MainActivity.this);
         super.loadApp(appKey);
       }
     };
