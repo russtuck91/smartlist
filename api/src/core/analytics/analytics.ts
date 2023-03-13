834e18e6d5fb112427c031adf6ac02b0bbafe1c1
Russell Tucker <russtuck91@gmail.com>
2023-03-12 22:12:05 -0400

    Revert server side Google Analytics to v3

--- api/src/core/analytics/analytics.ts
+++ api/src/core/analytics/analytics.ts
@@ -8,6 +8,9 @@ const analytics = Analytics({
     app: 'smartlist',
     plugins: [
         googleAnalytics({
+            // trackingId is for GA v3 aka Universal Analytics - supposedly reaching EOL on 7/1/23
+            trackingId: 'UA-188450923-1',
+            // measurementIds is for GA v4 - not currently working server-side
             measurementIds: [trackingId],
         }),
     ],
