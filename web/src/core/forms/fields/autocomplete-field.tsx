5204287d73688233301e402cdfee654d0981e642
Russell Tucker <russtuck91@gmail.com>
2023-04-14 21:24:49 -0400

    Use react-query in AutocompleteField

--- web/src/core/forms/fields/autocomplete-field.tsx
+++ web/src/core/forms/fields/autocomplete-field.tsx
@@ -1,7 +1,8 @@
 import { TextField as MUITextField, Theme, WithStyles, withStyles } from '@material-ui/core';
 import { Autocomplete, RenderOptionState } from '@material-ui/lab';
-import { debounce } from 'lodash';
-import React, { useCallback, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { useDebounce } from 'use-debounce';
 
 import { requests } from '../../requests/requests';
 
@@ -45,17 +46,29 @@ const useStyles = (theme: Theme) => ({
 type FullProps = AutocompleteInputProps & WithStyles<typeof useStyles>;
 
 function RawAutocompleteInput(props: FullProps) {
-    const [textFieldValue, setTextFieldValue] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [options, setOptions] = useState<any[]>([]);
-    const [fetchError, setFetchError] = useState<Error|null>(null);
+    const [inputValue, setInputValue] = useState('');
+    const [debouncedInputValue] = useDebounce(inputValue, 500);
     const [open, setOpen] = useState(false);
 
+    const searchUrl = props.getSearchUrl(debouncedInputValue);
+    const query = useQuery({
+        queryKey: ['autocomplete', debouncedInputValue, searchUrl],
+        queryFn: async () => {
+            if (!debouncedInputValue) {
+                return [];
+            }
+            return await requests.get(searchUrl);
+        },
+        placeholderData: [],
+        staleTime: (60 * 60 * 1000),
+        refetchOnWindowFocus: false,
+    });
+
     function getErrorText() {
-        if (loading) {
+        if (query.isLoading) {
             return null;
         }
-        if (fetchError) {
+        if (query.isError) {
             return 'There was a problem fetching results. Please try again.';
         }
         if (typeof props.error === 'boolean') {
@@ -82,41 +95,17 @@ function RawAutocompleteInput(props: FullProps) {
 
     function handleInputChange(e: React.ChangeEvent<unknown>|null, newInputValue: string) {
         if (e?.type === 'blur' && !newInputValue) { return; }
-        setTextFieldValue(newInputValue);
-        setFetchError(null);
-        searchByText(newInputValue);
+        setInputValue(newInputValue);
     }
-
-    const searchByText = useCallback(debounce(
-        async (text: string) => {
-            if (!text) {
-                return;
-            }
-            setLoading(true);
-            try {
-                const searchUrl = props.getSearchUrl(text);
-                const result = await requests.get(searchUrl);
-                setLoading(false);
-                setFetchError(null);
-                setOptions(result);
-            } catch (e) {
-                console.error(e);
-                setLoading(false);
-                setFetchError(e);
-                handleClose();
-            }
-        },
-        500,
-    ), []);
 
     return (
         <Autocomplete
             id={props.id}
             value={props.value}
-            inputValue={textFieldValue}
+            inputValue={inputValue}
             onInputChange={handleInputChange}
-            loading={loading}
-            options={options}
+            loading={query.isFetching}
+            options={query.data}
             getOptionLabel={props.getOptionLabel}
             renderOption={props.renderOption}
             onChange={handleChange}
