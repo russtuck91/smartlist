c0f823a9bc3ffc580b507db361e6d2b6bd9bba70
Russell Tucker <russtuck91@gmail.com>
2023-09-21 12:10:30 -0400

    Allow any user custom content on genre autocomplete field - fix freeSolo onChange logic

--- web/src/core/forms/fields/autocomplete-field.tsx
+++ web/src/core/forms/fields/autocomplete-field.tsx
@@ -80,7 +80,7 @@ function RawAutocompleteInput(props: FullProps) {
         return props.error;
     }
 
-    function handleChange(event: React.ChangeEvent<any>, value: any) {
+    function handleChange(event: React.ChangeEvent<any>|null, value: any) {
         if (!props.setFieldValue) {
             return;
         }
@@ -99,6 +99,7 @@ function RawAutocompleteInput(props: FullProps) {
     function handleInputChange(e: React.ChangeEvent<unknown>|null, newInputValue: string) {
         if (e?.type === 'blur' && !newInputValue) { return; }
         setInputValue(newInputValue);
+        if (props.freeSolo) handleChange(e, newInputValue);
     }
 
     return (
