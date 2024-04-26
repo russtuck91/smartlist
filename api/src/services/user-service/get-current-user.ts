955423cac13f41bf54e423df414d3a13a9163f0b
Russell Tucker <russtuck91@gmail.com>
2024-04-26 18:19:37 -0400

    Fix logging in getCurrentUser

--- api/src/services/user-service/get-current-user.ts
+++ api/src/services/user-service/get-current-user.ts
@@ -2,7 +2,6 @@ import httpContext from 'express-http-context';
 import { Unauthorized } from 'http-errors';
 
 import logger from '../../core/logger/logger';
-import maskToken from '../../core/logger/mask-token';
 import { User } from '../../core/session/models';
 
 import userRepo from '../../repositories/user-repository';
@@ -10,8 +9,8 @@ import userRepo from '../../repositories/user-repository';
 
 async function getCurrentUser(): Promise<User> {
     const sessionToken = httpContext.get('sessionToken');
-    logger.info('>>>> In getCurrentUser, typeof sessionToken', typeof sessionToken);
-    logger.info(`sessionToken = ${maskToken(sessionToken)}`);
+    logger.info(`>>>> In getCurrentUser, typeof sessionToken ${typeof sessionToken}`);
+    logger.info(`sessionToken = ${(sessionToken)}`);
     const currentUser = await userRepo.findOne({ sessionToken: sessionToken });
     logger.info('current user found...');
     logger.info(JSON.stringify(currentUser));
