dec7f91b72664076697c90615ccd19d9d2623ee5
Russell Tucker <russtuck91@gmail.com>
2024-05-16 21:11:14 -0400

    Extract agenda logic to startAgenda

--- api/src/agenda.ts
+++ api/src/agenda.ts
@@ -9,35 +9,32 @@ import logger from './core/logger/logger';
 import JobTypes from './jobs/job-types';
 import { publishAllPlaylists } from './services/playlist-service';
 
-const connectionOpts = { db: { address: MONGODB_URI, collection: 'jobs' } };
 
+const connectionOpts = { db: { address: MONGODB_URI, collection: 'jobs' } };
 export const agenda = new Agenda(connectionOpts);
 
 const jobTypes = convertEnumToArray(JobTypes);
-
 jobTypes.forEach((type) => {
     const module = require(`./jobs/${ kebabCase(type)}`);
     module.default(agenda);
 });
 
-if (jobTypes.length) {
-    (async () => {
-        logger.info('Starting agenda');
-        await agenda.start();
-        logger.info('Started agenda');
+export async function startAgenda() {
+    logger.info('Starting agenda');
+    await agenda.start();
+    logger.info('Started agenda');
 
-        if (process.env.NODE_ENV !== 'development') {
-            // await agenda.every('1 hour', JobTypes.playlistPublishing);
+    if (process.env.NODE_ENV !== 'development') {
+        // await agenda.every('1 hour', JobTypes.playlistPublishing);
 
-            // Try the dumb, native way
-            const PER_HOUR = 1000 * 60 * 60;
-            setInterval(() => {
-                publishAllPlaylists();
-            }, PER_HOUR);
-        }
+        // Try the dumb, native way
+        const PER_HOUR = 1000 * 60 * 60;
+        setInterval(() => {
+            publishAllPlaylists();
+        }, PER_HOUR);
+    }
 
-        await agenda.now(JobTypes.playlistPublishing);
-    })();
+    await agenda.now(JobTypes.playlistPublishing);
 }
 
 async function graceful() {
