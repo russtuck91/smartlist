793e080faab842d50c892f87ee4833a5a19892f0
Russell Tucker <russtuck91@gmail.com>
2024-06-24 00:48:39 -0400

    Add actions and data to playlist deleted notification

--- api/src/services/playlist-service/send-playlist-deleted-notification.ts
+++ api/src/services/playlist-service/send-playlist-deleted-notification.ts
@@ -3,13 +3,21 @@ import { Playlist } from '../../../../shared';
 import { sendNotification } from '../subscription-service';
 
 function sendPlaylistDeletedNotification(playlist: Playlist) {
-    const { userId, name: playlistName } = playlist;
+    const { userId, name: playlistName, id: playlistId } = playlist;
+    const publishUrl = `/playlists/publish/${playlistId}`;
 
     sendNotification({
         userId,
         notification: {
             title: 'Playlist deleted from Spotify',
             body: `The smart playlist ${playlistName} was deleted from Spotify. If this was an accident, click Publish to restore it.`,
+            actions: [{
+                title: 'Publish',
+                action: publishUrl,
+            }],
+            data: {
+                url: publishUrl,
+            },
         },
     });
 }
