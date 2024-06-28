30c4e743fb7f7929950ae142b27f69fcd9bc74b9
Russell Tucker <russtuck91@gmail.com>
2024-06-27 21:12:33 -0400

    Update pwa badge and icon

--- web/public/sw-handle-push.js
+++ web/public/sw-handle-push.js
@@ -7,8 +7,14 @@ self.addEventListener('push', function(e) {
     const notificationTitle = notificationData.title;
     const notificationOptions = {
         body: notificationData.body,
+
+        // https://pushpad.xyz/blog/web-notifications-difference-between-icon-image-badge
+        // `badge` is a very small icon that is usually displayed in the notification bar
         badge: notificationData.image || 'pwa-notification-badge.png',
-        icon: notificationData.image || 'favicon.ico',
+        // `icon` usually represents the sender
+        icon: notificationData.image || 'android-chome-192x192.png',
+        // `image` represents the main content of the notification
+
         actions: notificationData.actions,
         data: notificationData.data,
     };
