705ff67c8b1f2f18ecd212189fad07e238915220
Russell Tucker <russtuck91@gmail.com>
2024-07-01 01:04:03 -0400

    Fail publishPlaylist transaction on exception

--- api/src/services/playlist-service/publish-playlist.ts
+++ api/src/services/playlist-service/publish-playlist.ts
@@ -18,37 +18,45 @@ async function publishPlaylist(playlist: Playlist, accessToken: string) {
         name: 'Publish Playlist',
     });
 
-    const list = await populateList(playlist, accessToken);
+    try {
+        const list = await populateList(playlist, accessToken);
 
-    let spotifyPlaylistId = playlist.spotifyPlaylistId;
-    const userHasPlaylist = spotifyPlaylistId && await spotifyService.userHasPlaylist(spotifyPlaylistId, accessToken);
-    if (spotifyPlaylistId && userHasPlaylist) {
-        // Remove all tracks from playlist
-        await spotifyService.removeTracksFromPlaylist(spotifyPlaylistId, accessToken);
-    } else {
-        const newPlaylist = await spotifyService.createNewPlaylist(playlist.name, playlist.userId);
-        spotifyPlaylistId = newPlaylist.id;
+        let spotifyPlaylistId = playlist.spotifyPlaylistId;
+        const userHasPlaylist = spotifyPlaylistId && await spotifyService.userHasPlaylist(spotifyPlaylistId, accessToken);
+        if (spotifyPlaylistId && userHasPlaylist) {
+            // Remove all tracks from playlist
+            await spotifyService.removeTracksFromPlaylist(spotifyPlaylistId, accessToken);
+        } else {
+            const newPlaylist = await spotifyService.createNewPlaylist(playlist.name, playlist.userId);
+            spotifyPlaylistId = newPlaylist.id;
+        }
+
+        // Add tracks to playlist
+        const trackUris = list.map((i) => i.uri);
+        await spotifyService.addTracksToPlaylist(spotifyPlaylistId, trackUris, accessToken);
+
+        const playlistDetailsUpdates = {
+            name: playlist.name,
+        };
+        await spotifyService.updatePlaylistDetails(spotifyPlaylistId, playlistDetailsUpdates, accessToken);
+
+        // Save last published date, spotifyPlaylistId (in case it changed)
+        const playlistUpdate: Partial<Playlist> = {
+            spotifyPlaylistId: spotifyPlaylistId,
+            lastPublished: new Date(),
+            deleted: false,
+        };
+        await updatePlaylist(playlist.id, playlistUpdate);
+
+        transaction.finish();
+        logger.info(`<<<< Exiting publishPlaylist after successful publish. playlist.id = ${playlist.id} /// accessToken = ${maskToken(accessToken)}`);
+    } catch (e) {
+        logger.error('Error in publishPlaylist()');
+        logger.error(e);
+        transaction.setStatus('internal_error');
+        transaction.finish();
+        throw e;
     }
-
-    // Add tracks to playlist
-    const trackUris = list.map((i) => i.uri);
-    await spotifyService.addTracksToPlaylist(spotifyPlaylistId, trackUris, accessToken);
-
-    const playlistDetailsUpdates = {
-        name: playlist.name,
-    };
-    await spotifyService.updatePlaylistDetails(spotifyPlaylistId, playlistDetailsUpdates, accessToken);
-
-    // Save last published date, spotifyPlaylistId (in case it changed)
-    const playlistUpdate: Partial<Playlist> = {
-        spotifyPlaylistId: spotifyPlaylistId,
-        lastPublished: new Date(),
-        deleted: false,
-    };
-    await updatePlaylist(playlist.id, playlistUpdate);
-
-    transaction.finish();
-    logger.info(`<<<< Exiting publishPlaylist after successful publish. playlist.id = ${playlist.id} /// accessToken = ${maskToken(accessToken)}`);
 }
 
 export default publishPlaylist;
