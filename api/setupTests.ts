9e99ffd84749d28773f8ec0347b3c0e32a048c5a
Russell Tucker <russtuck91@gmail.com>
2024-07-05 18:59:32 -0400

    Handle base62 error in addTracksToPlaylist

--- api/setupTests.ts
+++ api/setupTests.ts
@@ -1,5 +1,7 @@
+require('dotenv').config();
 
 
 jest.mock('mongtype');
 jest.mock('agenda');
+jest.mock('spotify-web-api-node');
 
--- api/src/services/spotify-service/add-tracks-to-playlist.ts
+++ api/src/services/spotify-service/add-tracks-to-playlist.ts
@@ -4,22 +4,43 @@ import logger from '../../core/logger/logger';
 
 import doAndWaitForRateLimit from './do-and-wait-for-rate-limit';
 import initSpotifyApi from './init-spotify-api';
+import { isSpotifyError } from './types';
 
+
+// Spotify API requires batches of 100 max
+export const batchSize = 100;
 
 async function addTracksToPlaylist(playlistId: string, trackUris: string[], accessToken: string|undefined) {
     logger.debug(`>>>> Entering addTracksToPlaylist(playlistId = ${playlistId}`);
 
-    // Spotify API requires batches of 100 max
-    const batchSize = 100;
     const batchedUris = chunk(trackUris, batchSize);
 
     const spotifyApi = await initSpotifyApi(accessToken);
 
     for (const uriBatch of batchedUris) {
-        await doAndWaitForRateLimit(async () =>
-            await spotifyApi.addTracksToPlaylist(playlistId, uriBatch),
-        );
+        try {
+            await doAndWaitForRateLimit(async () =>
+                await spotifyApi.addTracksToPlaylist(playlistId, uriBatch),
+            );
+        } catch (e) {
+            logger.info(`Error in addTracksToPlaylist batch(playlistId = ${playlistId}`);
+            logger.info(uriBatch);
+            handleError(e);
+        }
     }
+}
+
+function handleError(e) {
+    if (isSpotifyError(e)) {
+        if (e.statusCode === 400) {
+            if (e.body.error?.message === 'Invalid base62 id') {
+                logger.info('Error was an invalid base62');
+                return;
+            }
+        }
+    }
+    logger.error(e);
+    throw e;
 }
 
 export default addTracksToPlaylist;
