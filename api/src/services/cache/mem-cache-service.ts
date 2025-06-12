1cad30aaed06773cc8f8a4b70f7c1b995689d275
Russell Tucker <russtuck91@gmail.com>
2025-06-12 16:46:53 -0400

    Implement LRU strategy in MemCacheService

--- api/src/services/cache/mem-cache-service.ts
+++ api/src/services/cache/mem-cache-service.ts
@@ -5,19 +5,21 @@ import { SourceMethod } from './types';
 
 
 class MemCacheService<Resource extends CacheableResource> {
-    private store: Record<string, Resource> = {};
+    private store: Map<string, Resource> = new Map();
     private sourceMethod: SourceMethod<Resource>;
+    private maxSize: number;
     setItems?: (items: Resource[]) => Promise<void>;
 
-    constructor(sourceMethod: SourceMethod<Resource>) {
+    constructor(sourceMethod: SourceMethod<Resource>, maxSize: number = 100_000) {
         this.sourceMethod = sourceMethod;
+        this.maxSize = maxSize;
     }
 
     getItems = async (ids: string[], accessToken: string|undefined): Promise<Resource[]> => {
         logger.debug('>>>> Entering MemCacheService.getItems()');
 
         // Any IDs not found in memory need to be fetched
-        const idsToFetch = ids.filter((id) => !this.store[id]);
+        const idsToFetch = ids.filter((id) => !this.store.has(id));
         logger.debug(`Total requested: ${ids.length} /// Not in memory, will be fetched: ${idsToFetch.length}`);
 
         if (idsToFetch.length > 0) {
@@ -25,17 +27,43 @@ class MemCacheService<Resource extends CacheableResource> {
             const fetchedItems = await this.sourceMethod(idsToFetch, accessToken);
 
             // Save to memory cache
-            fetchedItems.reduce((agg, item) => {
-                agg[item.id] = item;
-                return agg;
-            }, this.store);
+            for (const item of fetchedItems) {
+                this.set(item.id, item);
+            }
         }
 
-        const result = ids.map((id) => this.store[id]!);
+        // Update usage order for all requested IDs (even if already present)
+        ids.forEach((id) => {
+            if (this.store.has(id)) {
+                const value = this.store.get(id)!;
+                this.store.delete(id);
+                this.store.set(id, value);
+            }
+        });
 
+        const result = ids.map((id) => this.store.get(id)!);
+
+        logger.debug(`MemCacheService store size: ${this.store.size}`);
         logger.debug('<<<< Exiting MemCacheService.getItems()');
         return result;
     };
+
+    /**
+     * Set an item in the cache, evicting LRU if necessary
+     */
+    private set(key: string, value: Resource) {
+        if (this.store.has(key)) {
+            this.store.delete(key);
+        }
+        this.store.set(key, value);
+
+        // LRU - Evict least recently used if over max size
+        if (this.store.size > this.maxSize) {
+            const lruKey = this.store.keys().next().value;
+            this.store.delete(lruKey);
+            logger.debug(`MemCacheService - Evicted LRU cache key: ${lruKey}`);
+        }
+    }
 }
 
 export default MemCacheService;
