8108c215dfb56c42aeaad14abb3037a579eaed46
Russell Tucker <russtuck91@gmail.com>
2026-04-21 11:36:39 -0400

    Revert adding manual versionCode

--- native/app.config.js
+++ native/app.config.js
@@ -18,7 +18,6 @@ export default (() => {
             ],
             'android': {
                 'package': `com.smartlistmusic.smartlist${APP_VARIANT ? `.${APP_VARIANT}` : ''}`,
-                'versionCode': 160,
                 'adaptiveIcon': {
                     'foregroundImage': './assets/images/android-icon-foreground.png',
                     'backgroundColor': brandGreen,
